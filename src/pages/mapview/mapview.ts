2cc94a8efd4918aa47003f327c8259327a797efc
Ogo Elumeze <ogo@clearlyinnovative.com>
2018-06-07 05:43:25 -0400

    initial

--- src/pages/mapview/mapview.ts
+++ src/pages/mapview/mapview.ts
@@ -9,7 +9,7 @@ import { Geolocation } from 'ionic-native';
 })
 export class LeafletView {
 
-  private _currentLatLng: any;
+  //private _currentLatLng: any;
   private _map: any;
 
   constructor(public navCtrl: NavController) {
@@ -21,21 +21,55 @@ export class LeafletView {
   }
 
   loadMap(){
-    Geolocation.getCurrentPosition({ maximumAge: 60000, timeout: 30000, enableHighAccuracy: true }).then((position) => {
- 
-        this._currentLatLng = [position.coords.latitude, position.coords.longitude];
-       
-        this._map = Leaflet
-          .map("map")
-          .setView(this._currentLatLng, 13)
-        
-        // this.map.options.zoomControl = false;
-
-        Leaflet.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}').addTo(this._map);
-
-     }, (error) => {
-      console.log(error);
+  
+    this._map = Leaflet.map("image-map", {
+      minZoom: 10,
+      maxZoom: 15,
+      center: [0, 0],
+      zoom: 10,
+      crs: Leaflet.CRS.Simple
     });
+
+    //dimensions of the image
+    let w = 3000,
+        h = 2500;
+    //  Leaflet.tileLayer('https://nmaahc.si.edu/sites/default/files/styles/image_caption/public/images/captioned/{z}/{x}/{y}/l4-900px_11-2.png?itok=4oFfcK-M', {
+    //   attribution : 'Map Sample',
+    // }).addTo(this._map)
+     let url = 'https://firebasestorage.googleapis.com/v0/b/nmaahc-df439.appspot.com/o/maps%2F3RX6gNFlwVRuuwcnNfw0DTbMhNt2_1473795412224_NMMAHC_FloorC2_v3.png?alt=media&token=f71de1de-abf4-421d-952e-2bd4dcfa709e';
+    
+    let southWest = this._map.unproject([0, h], this._map.getMaxZoom());
+    let northEast = this._map.unproject([w,0], this._map.getMaxZoom());
+    //var bounds : any = [[0,0], [w,h]];
+    let bounds = Leaflet.latLngBounds(southWest, northEast);
+
+   
+    Leaflet.imageOverlay(url, bounds).addTo(this._map);
+
+    this._map.setMaxBounds(bounds);
+    //this._map.fitBounds(bounds);
+
+
+    //     var map = L.map('image-map', {
+    //   minZoom: 1,
+    //   maxZoom: 4,
+    //   center: [0, 0],
+    //   zoom: 1,
+    //   crs: L.CRS.Simple
+    // });
+    // // dimensions of the image
+    // var w = 2000,
+    //     h = 1500,
+    //     url = 'http://kempe.net/images/newspaper-big.jpg';
+    // // calculate the edges of the image, in coordinate space
+    // var southWest = map.unproject([0, h], map.getMaxZoom()-1);
+    // var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
+    // var bounds = new L.LatLngBounds(southWest, northEast);
+    // // add the image overlay, 
+    // // so that it covers the entire map
+    // L.imageOverlay(url, bounds).addTo(map);
+    // // tell leaflet that the map is exactly as big as the image
+    // map.setMaxBounds(bounds);
   }
 
 }
